a53f1c26cb23ba5338c86a3ffc5fdc03db44036d
GH <81537944+herguad@users.noreply.github.com>
2024-10-23 19:38:41 -0300

    Update duration_stats.py

--- code/duration_stats.py
+++ code/duration_stats.py
@@ -14,12 +14,9 @@ netflix_subset = netflix_df[netflix_df["type"] == "Movie"]
 # Select only the columns of interest
 netflix_movies = netflix_subset[["title", "country", "genre", "release_year", "duration"]]
 
-###Duration###
-
 # Filter for durations shorter than 60 minutes
 short_movies = netflix_movies[netflix_movies.duration < 60]
 #print(short_movies.info)
-
 
 # Filter also for durations longer than 250 minutes to exclude outliers and movies released before 1960 (too few dataopint before that).
 netflix_movies=netflix_movies[(netflix_movies['duration'] >= 60) & (netflix_movies['duration'] < 250 ) & (netflix_movies['release_year'] > 1960)]
@@ -31,3 +28,4 @@ uncategorized=netflix_movies[netflix_movies['genre'] == 'Uncategorized']
 
 netflix_movies=netflix_movies.drop(index=[1318, 1320, 1570, 1709, 2177, 2178, 3253, 3736, 3737, 3738, 4187, 5576, 5577, 6735, 7170, 7171])
 
+duration_year=netflix_movies[['genre','release_year','duration']]
