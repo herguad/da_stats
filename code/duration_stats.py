ac12055eee53ac66151b9464c0fe870f394b54df
GH <81537944+herguad@users.noreply.github.com>
2024-10-23 19:38:03 -0300

    Update duration_stats.py

--- code/duration_stats.py
+++ code/duration_stats.py
@@ -21,14 +21,13 @@ short_movies = netflix_movies[netflix_movies.duration < 60]
 #print(short_movies.info)
 
 
-# Filter also for durations longer than 250 minutes to exclude outliers.
-netflix_movies=netflix_movies[(netflix_movies['duration'] >= 60) & (netflix_movies['duration'] < 250 )]
-#print(netflix_movies.info)
+# Filter also for durations longer than 250 minutes to exclude outliers and movies released before 1960 (too few dataopint before that).
+netflix_movies=netflix_movies[(netflix_movies['duration'] >= 60) & (netflix_movies['duration'] < 250 ) & (netflix_movies['release_year'] > 1960)]
+print(netflix_movies.info)
 
 #Filter out uncategorized movies.
 uncategorized=netflix_movies[netflix_movies['genre'] == 'Uncategorized']
 #print(uncategorized.index)
 
 netflix_movies=netflix_movies.drop(index=[1318, 1320, 1570, 1709, 2177, 2178, 3253, 3736, 3737, 3738, 4187, 5576, 5577, 6735, 7170, 7171])
-
 
