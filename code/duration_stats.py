f73a51d8dc944a183572ed327d43e57e5afd2a8c
herguad <g.s.herrera.s@gmail.com>
2024-11-02 15:08:39 -0300

    another commit

---
+++ code/duration_stats.py
@@ -0,0 +1,151 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import missingno as msno
+import seaborn as sn
+import statsmodels.api as sm
+
+# Read in the Netflix CSV as a DataFrame
+netflix_df = pd.read_csv('netflix_data.csv')
+#print(netflix_df.shape)
+#print(netflix_df.info())
+#print(netflix_df.head(3))
+
+# Subset the DataFrame for type "Movie"
+netflix_subset = netflix_df[netflix_df["type"] == "Movie"]
+
+# Select only the columns of interest
+netflix_movies = netflix_subset[["title", "country", "genre", "release_year", "duration"]]
+
+###Duration###
+
+# Filter for durations shorter than 60 minutes
+short_movies = netflix_movies[netflix_movies.duration < 60]
+#print(short_movies.info)
+
+
+# Filter also for durations longer than 250 minutes to exclude outliers and movies released before 1960 (too few dataopint before that).
+netflix_movies=netflix_movies[(netflix_movies['duration'] >= 60) & (netflix_movies['duration'] < 250 ) & (netflix_movies['release_year'] >= 1970)]
+#print(netflix_movies.info)
+
+#Filter out uncategorized movies.
+uncategorized=netflix_movies[netflix_movies['genre'] == 'Uncategorized']
+#print(uncategorized.index)
+
+netflix_movies=netflix_movies.drop(index=[1318, 1320, 1570, 1709, 2177, 2178, 3253, 3736, 3737, 3738, 4187, 5576, 5577, 6735, 7170, 7171])
+
+duration_year=netflix_movies[['genre','release_year','duration']]
+
+
+#Check for null data.
+
+#Where there years where no movies were released?
+all_years=range(1970,2020)
+years_movies=duration_year["release_year"].isin(all_years)
+no_movies_y=years_movies[years_movies==False]
+
+#print(years_movies.value_counts())
+
+#print(len(no_movies_y.index))
+drops=list(no_movies_y.index)
+
+#Drop those rows and reset index.
+duration_year=duration_year.drop(index=drops).reset_index(drop=True)
+
+#Check.
+print(duration_year.shape)
+
+#Remove rows with other null elements
+#print(duration_year.iloc[:,2].index)
+
+for i in range(0,2):
+    for c in duration_year.iloc[:,i].index:
+        if (duration_year.iloc[c,i] == '') is True:
+            duration_year.drop(index=c)
+        else:
+            continue
+        
+print(len(duration_year))
+print(duration_year.head())
+
+# Visualize missingness
+msno.matrix(duration_year)
+plt.show()
+
+#Check categorical variable consistency.
+genres=duration_year['genre'].value_counts()
+#print(genres)
+
+#Plot general distribution in a histogram.
+palette=sn.color_palette("Spectral", as_cmap=True,n_colors=180)
+fig=sn.histplot(duration_year,x='release_year',y='duration',palette=palette,hue='duration',legend =False,binwidth=1,discrete=(True, False))
+fig.set(xlabel="Release year",ylabel="Duration (min)")
+fig.set(title="Movie duration by year of release")
+fig.tick_params(labelsize=7)
+plt.show()
+
+#Remove 'Independent movies', 'International movies','Classic Movies' and 'Cult movies'
+no_mov=['International Movies','Classic Movies','Independent Movies', 'Cult Movies']
+
+for j in no_mov:
+    if str(j) in duration_year[['genre']].values:
+        print(j)
+        k=duration_year[duration_year['genre'] == j].index
+        print(k)
+    else:
+        continue
+
+
+classic=[ 114,  246,  247,  255,  310,  481,  487,  518,  640,  757,  851,  965,
+       1263, 1265, 1322, 1444, 1465, 1482, 1661, 1678, 1682, 2022, 2104, 2121,
+       2174, 2375, 2492, 2778, 2826, 2872, 2944, 2984, 2990, 3004, 3206, 3439,
+       3441, 3514, 3526, 3687, 3736, 3854, 3861, 3895, 3943, 3959, 4428, 4465]
+
+cult=[746, 818, 1051, 1271, 2278, 2748, 3508, 3657, 3829, 4083, 4542]
+
+independent=[  10,  435,  525,  630,  688,  711,  720,  825,  977,  979, 1134, 1186,
+       1333, 1451, 1751, 2134, 2402, 3653, 3734]
+
+indeces=classic+cult+independent
+dur_year=duration_year.drop(index=indeces)
+print(dur_year.shape)
+
+international=dur_year[dur_year['genre'] == 'International Movies'].index
+dur_yea=dur_year.drop(index=international)
+print(dur_yea.shape)
+
+#Plot general distribution for clean df in a histogram. 
+palette=sn.color_palette("Spectral", as_cmap=True,n_colors=180)
+fig2=sn.histplot(dur_yea,x='release_year',y='duration',palette=palette,hue='duration',legend =False,binwidth=1,discrete=(True, False))
+fig2.set(xlabel="Release year",ylabel="Duration (min)")
+fig2.set(title="Movie duration by year of release")
+fig2.tick_params(labelsize=7)
+plt.show()
+
+#Check remaining genres and amount of unique labels.
+genres=dur_yea['genre'].value_counts()
+print(genres)
+print(len(dur_yea['genre'].unique()))
+
+#Plot scatter  with genre as hue.
+palette=sn.color_palette("colorblind",n_colors=14)
+fig3=sn.scatterplot(dur_yea,x='release_year',y='duration',palette=palette,hue='genre',legend =False)
+fig3.set(xlabel="Release year",ylabel="Duration (min)")
+fig3.set(title="Movie duration by year of release")
+fig3.tick_params(labelsize=7)
+plt.show()
+
+print(dur_yea['duration'].describe())
+
+
+fig4=sn.regplot(dur_yea,x='release_year',y='duration',x_estimator=np.mean,color=(255/255, 138/255, 101/255,0.8),line_kws={'color':'royalblue'})
+fig4.set(xlabel="Release year",ylabel="Duration (min)")
+fig4.set(title="Movie duration by year of release")
+fig4.tick_params(labelsize=7)
+plt.show()
+
+#Check period of time covered--> 50 years
+data_year=dur_yea['release_year'].unique()
+print(len(data_year))
+
---
+++ code/genre_stats.py
@@ -0,0 +1,127 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import missingno as msno
+import seaborn as sn
+#import statsmodels.api as sm
+
+# Read in the Netflix CSV as a DataFrame
+netflix_df = pd.read_csv('netflix_data.csv')
+#print(netflix_df.shape)
+#print(netflix_df.info())
+#print(netflix_df.head(3))
+
+# Subset the DataFrame for type "Movie"
+netflix_subset = netflix_df[netflix_df["type"] == "Movie"]
+
+# Select only the columns of interest
+netflix_movies = netflix_subset[["title", "country", "genre", "release_year", "duration"]]
+
+###Genre###
+#Select columns of interest. Transform date column into datetype and extract year from dates and re-write cdate_added column to show just years.
+movies_sel=netflix_subset.loc[:,('country','date_added','genre','duration')]
+movies_sel.loc[:,('date_added')]=pd.to_datetime(movies_sel.loc[:,('date_added')])
+movies_sel['year_added']=movies_sel['date_added'].values  #Ignore warning but read: https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
+movies_sel['year_added']=movies_sel['year_added'].dt.year
+
+#Add different dfs selecting release_year and genre and genre separately
+genre_year=movies_sel.loc[:,('genre','year_added','duration')]
+movies_country_genre=movies_sel.loc[:,('genre','duration')]
+
+#Group movies by genre and year added, and count genres movies added per year. 
+m_genre_year=genre_year.groupby(["genre","duration"]).value_counts(ascending=True).reset_index(name='genreyear')
+#print(m_genre_year)
+
+#Count uncategorized movies and remove.
+#Stats based on years when movies were added.
+uncategorized_s=m_genre_year[m_genre_year['genre']=='Uncategorized']['genreyear'].sum()
+uncategorized=m_genre_year[m_genre_year['genre']=='Uncategorized']
+#print(len(uncategorized))
+
+#Separate international movies into a different df and remove from main.
+international_movies_g= genre_year[genre_year['genre']=='International Movies']
+#print(international_movies_g.index)
+
+genre_year=m_genre_year.drop(index=[74, 75, 76, 77, 78, 79,107,108,109,110,111,112,113]).reset_index(drop=True)
+genre_year=genre_year.sort_values(by='year_added',ascending=True)
+
+print(genre_year) #--> check n of rows is  down.
+
+print(genre_year['genre'].unique()) # <-  distinct genres after cleaning
+
+#Check categorical variable consistency.
+genre_sum=genre_year['genre'].value_counts()
+print(genre_sum)
+
+
+#Remove 'Independent movies', 'International movies','Classic Movies' and 'Cult movies'
+no_mov=['Classic Movies','Independent Movies', 'Cult Movies', 'Uncategorized']
+
+for j in no_mov:
+    if str(j) in genre_year[['genre']].values:
+        print(j)
+        k=genre_year[genre_year['genre'] == j].index
+        print(k)
+    else:
+        continue
+
+classic=[24, 25, 26, 27, 28, 29]
+
+cult=[38, 39, 40, 41, 42]
+
+independent=[70, 71, 72, 73]
+
+uncategorized=[2328, 2314, 2317, 2337, 2327, 2308, 2335, 2324, 2312, 2333, 2348, 2306,
+       2316, 2340, 2332, 2302, 2305, 2321, 2344, 2350, 2341, 2342, 2330, 2345,
+       2346, 2331, 2322, 2325, 2326, 2347, 2311, 2303, 2309, 2307, 2313, 2339,
+       2338, 2349, 2320, 2323, 2315, 2304, 2334, 2329, 2336, 2318, 2343, 2319,
+       2310, 2351]
+
+indeces=classic+cult+independent+uncategorized
+gen_year=genre_year.drop(index=indeces).sort_values(by='duration',ascending=True)
+print(gen_year.shape)
+
+
+# Visualize missingness
+msno.matrix(gen_year)
+plt.show()
+
+palette=sn.color_palette("colorblind",n_colors=18)
+fig=sn.catplot(gen_year,x='genre',y='duration',palette=palette,hue='genre',legend =False,kind='bar')
+fig.set(xlabel="Release year",ylabel="Duration (min)")
+fig.set(title="Movie duration by year of release")
+fig.tick_params(labelsize=7)
+plt.show()
+
+
+#Check categorical variable consistency.
+
+genre_sum=genre_year.groupby('genre')['genre'].count().reset_index(name='mcount')
+genre_sum=genre_sum.sort_values(by='mcount',ascending=False)
+print(genre_sum)
+
+
+#filter out genres with fewer than 11 observations
+genre_sum=genre_sum[genre_sum['mcount'] > 11].reset_index(drop=True)
+print(genre_sum)
+
+
+no_mov=['Classic Movies','Independent Movies', 'Cult Movies', 'Uncategorized']
+
+for j in no_mov:
+    if str(j) in genre_sum[['genre']].values:
+        print(j)
+        k=genre_sum[genre_sum['genre'] == j].index
+        print(k)
+    else:
+        continue
+
+gen_sum=genre_sum.drop(index=[8,9,11,14]).sort_values(by='mcount',ascending=True)
+
+palette=sn.color_palette("colorblind",n_colors=18)
+fig=sn.regplot(genre_sum,x='genre',y='mcount',palette=palette,hue='genre',legend =False,kind='bar')
+fig.set(xlabel="Gnere",ylabel="Total movies")
+fig.set(title="Movie duration by year of release")
+fig.tick_params(labelsize=7)
+plt.show()
